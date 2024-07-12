5c82cfdf4afe42012bb9219c2eb1f9c733ed63eb
Siripireddy, Hemanth <Hemanth.Siripireddy@verint.com>
2024-07-12 18:46:57 +0530

    added all CRUD REST Methods

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/hemanth/crudedemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/hemanth/crudedemo/rest/EmployeeRestController.java
@@ -13,26 +13,54 @@ import java.util.List;
 public class EmployeeRestController {
 
     private EmployeeService employeeService;
+
     @Autowired
-    public EmployeeRestController(EmployeeService employeeService){
-        this.employeeService=employeeService;
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
+
     @GetMapping("/employees")
-    public List<Employee> getEmployees(){
+    public List<Employee> getEmployees() {
         return employeeService.findAll();
 
     }
+
     @GetMapping("/employees/{studentId}")
-    public Employee getById(@PathVariable  int studentId){
-        Employee employee= employeeService.findById(studentId);
-        if(employee==null)
-            throw  new RuntimeException("Employee not found with id: "+studentId);
+    public Employee getById(@PathVariable int studentId) {
+        Employee employee = employeeService.findById(studentId);
+
+        if (employee == null)
+            throw new RuntimeException("Employee not found with id: " + studentId);
         return employee;
 
     }
+
     @PostMapping("/employees")
-    public Employee saveEmployee(@RequestBody  Employee employee){
-        Employee dbEmployee=employeeService.save(employee);
+    public Employee saveEmployee(@RequestBody Employee employee) {
+        employee.setId(0);
+        Employee dbEmployee = employeeService.save(employee);
         return dbEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+
+        Employee dbEmployee = employeeService.findById(employee.getId());
+        if (dbEmployee == null)
+            throw new RuntimeException("Employee not found with id: " + employee.getEmail() + " to update");
+        dbEmployee = employeeService.save(employee);
+        return dbEmployee;
+
+    }
+    @DeleteMapping("/employees/{employeeId}")
+    public String updateEmployee(@PathVariable int employeeId) {
+
+        Employee dbEmployee = employeeService.findById(employeeId);
+        if (dbEmployee == null)
+            throw new RuntimeException("Employee not found with id: " + employeeId + " to delete");
+        employeeService.deleteById(employeeId);
+
+        return "Employee Deleted with id: "+employeeId;
+
     }
 }
