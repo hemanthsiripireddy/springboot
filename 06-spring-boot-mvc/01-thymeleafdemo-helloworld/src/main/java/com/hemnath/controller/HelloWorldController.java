054c517eacdc69e93e23cac459a9c57d869bad87
Siripireddy, Hemanth <Hemanth.Siripireddy@verint.com>
2024-07-18 16:57:52 +0530

    spring mvc fomr office laptop

---
+++ 06-spring-boot-mvc/01-thymeleafdemo-helloworld/src/main/java/com/hemnath/controller/HelloWorldController.java
@@ -0,0 +1,41 @@
+package com.hemnath.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/showForm")
+    public String showForm(){
+        return "helloworld-form";
+    }
+    @RequestMapping("/processForm")
+    public String processForm(){
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutOut(HttpServletRequest request, Model model){
+
+        String name=request.getParameter("studentName");
+         name=name.toUpperCase();
+         String result="Yo! "+name;
+         model.addAttribute("message",result);
+        return "helloworld";
+    }
+    @PostMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String name, Model model){
+
+        //String name=request.getParameter("studentName");
+         name=name.toUpperCase();
+         String result="Hey My Friend! "+name;
+         model.addAttribute("message",result);
+        return "helloworld";
+    }
+}
